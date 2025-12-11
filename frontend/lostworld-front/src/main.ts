1e9cafd9b00d9b3a46fc50192f2a14f17d4a7115
Raul Ronald <108026640+RaulRonald@users.noreply.github.com>
2025-12-11 15:38:02 -0300

    atualização das rotas
    usando vue router para fazer as rotas principais do projeto

--- frontend/lostworld-front/src/main.ts
+++ frontend/lostworld-front/src/main.ts
@@ -2,13 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
-import {createRouter,createWebHashHistory} from  'vue-router'
-const pinia = createPinia()
+import router from './router'  
+
 const app = createApp(App)
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes:[]
-})
-app.use(pinia)
+
+app.use(createPinia())
 app.use(router)
+
 app.mount('#app')
---
+++ frontend/lostworld-front/src/views/PublishPostView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>PublishPost</h1>
+</template>

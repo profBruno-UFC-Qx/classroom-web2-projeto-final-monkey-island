5bdc32c1ce1a8b7edc51bc3c96d7b344f4bf81a2
Paulo Marcelo <marcelomp97447@gmail.com>
2025-12-20 11:58:41 -0300

    feat: add UserRepository in di lifecycle

--- backend/src/di/container.di.ts
+++ backend/src/di/container.di.ts
@@ -1,3 +1,9 @@
 import { Container } from "inversify";
+import { User } from "../entities/User";
+import { AppDataSource } from "../config/db.connection";
+import { TYPES } from "../types/types";
 
 export const container: Container = new Container();
+
+const UserRepositoryDB = AppDataSource.getRepository(User);
+container.bind(TYPES.UserRepositoryDB).toConstantValue(UserRepositoryDB);

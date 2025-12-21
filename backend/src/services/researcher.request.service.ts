efa7bc339e0b8a831bf130b9e2a46241765dfef8
Paulo Marcelo <marcelomp97447@gmail.com>
2025-12-21 11:43:43 -0300

    feat: adding create request method in ReseacherRequestService

--- backend/src/services/researcher.request.service.ts
+++ backend/src/services/researcher.request.service.ts
@@ -2,8 +2,10 @@ import { inject, injectable } from "inversify";
 import { RequestToBeResearcherDto } from "../dtos/researcher_request/request/RequestToBeResearcherDto";
 import { RequestToBeResearcherResponseDto } from "../dtos/researcher_request/response/RequestToBeResearcherResponseDto";
 import { TYPES } from "../types/types";
-import { UserService } from "./user.service";
+import { User } from "../entities/User";
+import { IUserService } from "./user.service";
 import { IResearcherRequestRepositorie } from "../repositories/researcher.request.repositorie";
+import { ResearcherRequest } from "../entities/researcher.request";
 
 export interface IResearcherRequestService {
   create(
@@ -23,9 +25,52 @@ export interface IResearcherRequestService {
 @injectable()
 export class ResearcherRequestService implements IResearcherRequestService {
   constructor(
-    @inject(TYPES.UserService) private userService: UserService,
+    @inject(TYPES.UserService) private userService: IUserService,
 
     @inject(TYPES.ResearcherRequestRepositoryDB)
     private researcherRequestRepository: IResearcherRequestRepositorie
   ) {}
+
+  async create(
+    userId: string,
+    dto: RequestToBeResearcherDto
+  ): Promise<RequestToBeResearcherResponseDto> {
+    const user = await this.userService.findUserById(userId);
+    const existsPendingRequest =
+      await this.researcherRequestRepository.existsPendingRequest(user);
+
+    if (existsPendingRequest) {
+      throw new Error("pending request already exists!");
+    }
+
+    const reseacherRequest = this.dtoToEntity(user, dto);
+    const dataResponse =
+      await this.researcherRequestRepository.save(reseacherRequest);
+    const response = this.entityToResponseDto(dataResponse);
+
+    return response;
+  }
+
+  private entityToResponseDto(
+    data: ResearcherRequest
+  ): RequestToBeResearcherResponseDto {
+    return {
+      id: data.id,
+      status: data.status,
+      user_id: data.user.id,
+      motivation: data.motivation,
+      updated_at: data.updatedAt,
+      created_at: data.createdAt,
+    };
+  }
+
+  private dtoToEntity(
+    user: User,
+    dto: RequestToBeResearcherDto
+  ): ResearcherRequest {
+    const researcherRequest = new ResearcherRequest();
+    researcherRequest.motivation = dto.motivation;
+    researcherRequest.user = user;
+    return researcherRequest;
+  }
 }

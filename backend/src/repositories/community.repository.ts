ca8c50e2a56336c7ee4df2c91957940a797c0e9c
Paulo Marcelo <marcelomp97447@gmail.com>
2025-12-29 10:30:47 -0300

    refactor: changing the name of the method to get the communities that the user owns

--- backend/src/repositories/community.repository.ts
+++ backend/src/repositories/community.repository.ts
@@ -4,9 +4,9 @@ import { Community } from "../entities/community";
 import { Like } from "typeorm";
 
 export interface ICommunityRepository {
-  save(community: Community): Promise<void>;
+  save(community: Community): Promise<Community>;
   deleteCommunity(communityId: string): Promise<void>;
-  getAllCommunitiesByUserId(userId: string): Promise<Community[]>;
+  getCommunitiesCreatedByUser(userId: string): Promise<Community[]>;
   findAllCommunitiesByNameLike(
     name: string,
     skip: number,

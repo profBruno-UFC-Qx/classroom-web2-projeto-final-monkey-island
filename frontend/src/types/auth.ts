d34e21d2abf2b6b45c5f99abcf8a00f7216aa2f8
Raul Ronald <108026640+RaulRonald@users.noreply.github.com>
2025-12-29 05:07:37 -0300

    configurando tipos e bootstrap

---
+++ frontend/src/types/auth.ts
@@ -0,0 +1,15 @@
+import type { User } from './user';
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface RegisterCredentials extends LoginCredentials {
+  name: string;
+}
+
+export interface AuthResponse {
+  token: string;
+  user: User;
+}

cfd2ad3efb029faf937ab164ad383be46f0dc15f
Paulo Marcelo <marcelomp97447@gmail.com>
2025-12-30 07:39:15 -0300

    feat: adding interface of user community repository  with abstract methods

--- backend/src/repositories/user.community.repository.ts
+++ backend/src/repositories/user.community.repository.ts
@@ -1,11 +1,29 @@
-import { CommunityUser } from "../entities/community.user";
+import { Community } from "../entities/community";
+import { CommunityUser, CommunityUserStatus } from "../entities/community.user";
+import { User } from "../entities/User";
 
 export interface IUserCommunityRepository {
   save(commityUser: CommunityUser): Promise<void>;
-  deleteByUserId(userId: string, communityId: string): Promise<void>;
   findUsersOfTheCommunity(
     communityId: string,
     skip: number,
     take: number
-  ): Promise<[]>;
+  ): Promise<[User[], number]>;
+  findUsersOfTheCommunityByStatus(
+    communityId: string,
+    status: CommunityUserStatus,
+    skip: number,
+    take: number
+  ): Promise<[User[], number]>;
+
+  findByUserAndCommunity(
+    userId: string,
+    communityId: string
+  ): Promise<CommunityUser | null>;
+
+  findCommunitiesByUserId(
+    userId: string,
+    skip: number,
+    take: number
+  ): Promise<[Community[], number]>;
 }

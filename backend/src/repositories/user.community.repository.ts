b7cf0564bc044154b69517e4bba7c52d3da5f57c
Paulo Marcelo <marcelomp97447@gmail.com>
2025-12-30 15:29:40 -0300

    feat: adding find users of community implementation in repository

--- backend/src/repositories/user.community.repository.ts
+++ backend/src/repositories/user.community.repository.ts
@@ -1,20 +1,33 @@
 import { Community } from "../entities/community";
 import { CommunityUser, CommunityUserStatus } from "../entities/community.user";
 import { User } from "../entities/User";
+import { AppDataSource } from "../config/db.connection";
+
+export interface UserCommunityData {
+  joinedAt: Date;
+  leftAt?: Date;
+  bannedAt?: Date;
+  suspendedAt?: Date;
+}
+
+export interface UserWithCommunityData {
+  user: User;
+  communityData: UserCommunityData;
+}
 
 export interface IUserCommunityRepository {
-  save(commityUser: CommunityUser): Promise<void>;
+  save(commityUser: CommunityUser): Promise<CommunityUser>;
   findUsersOfTheCommunity(
     communityId: string,
     skip: number,
     take: number
-  ): Promise<[User[], number]>;
+  ): Promise<[UserWithCommunityData[], number]>;
   findUsersOfTheCommunityByStatus(
     communityId: string,
     status: CommunityUserStatus,
     skip: number,
     take: number
-  ): Promise<[User[], number]>;
+  ): Promise<[UserWithCommunityData[], number]>;
 
   findByUserAndCommunity(
     userId: string,
@@ -26,4 +39,62 @@ export interface IUserCommunityRepository {
     skip: number,
     take: number
   ): Promise<[Community[], number]>;
+}
+
+export class UserCommunityRepositoryDB implements IUserCommunityRepository {
+  private get repo() {
+    return AppDataSource.getRepository(CommunityUser);
+  }
+
+  async save(commityUser: CommunityUser): Promise<CommunityUser> {
+    return await this.repo.save(commityUser);
+  }
+
+  private mapCollection(items: CommunityUser[]): UserWithCommunityData[] {
+    return items.map((item) => {
+      let communityData = { joinedAt: item.joinedAt };
+
+      let response: UserWithCommunityData = {
+        user: item.user,
+        communityData,
+      };
+
+      if (item.status === CommunityUserStatus.BANNED) {
+        response = {
+          ...response,
+          communityData: { ...communityData, bannedAt: item.bannedAt },
+        };
+      } else if (item.status === CommunityUserStatus.SUSPENDED) {
+        response = {
+          ...response,
+          communityData: { ...communityData, suspendedAt: item.suspendedAt },
+        };
+      } else if (item.status === CommunityUserStatus.INACTIVE) {
+        response = {
+          ...response,
+          communityData: { ...communityData, leftAt: item.leftAt },
+        };
+      }
+      return response;
+    });
+  }
+
+  async findUsersOfTheCommunity(
+    communityId: string,
+    skip: number,
+    take: number
+  ): Promise<[UserWithCommunityData[], number]> {
+    const [items, total] = await this.repo.findAndCount({
+      where: {
+        community: { id: communityId },
+        status: CommunityUserStatus.ACTIVE,
+      },
+      relations: ["user"],
+      skip,
+      take,
+    });
+
+    const mapped: UserWithCommunityData[] = this.mapCollection(items);
+    return [mapped, total];
+  }
 }

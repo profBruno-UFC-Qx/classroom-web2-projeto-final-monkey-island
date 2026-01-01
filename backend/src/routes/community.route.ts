06193741e0e7510e2ad741c5bbc12585a60a2b6e
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-01 19:18:53 -0300

    chore: create di to  community flow

---
+++ backend/src/routes/community.route.ts
@@ -0,0 +1,4 @@
+import { Router } from "express";
+import { container } from "../di/container.di";
+import { TYPES } from "../types/types";
+import { CommunityController } from "../controllers/community.controller";

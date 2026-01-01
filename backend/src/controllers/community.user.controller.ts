878ca081379ee408c0aef9d3c5b47d701645bfbd
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-01 15:38:02 -0300

    feat: adding join in group method in communityUser controller

---
+++ backend/src/controllers/community.user.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import { inject, injectable } from "inversify";
+import { TYPES } from "../types/types";
+
+import { ICommunityUserService } from "../services/community.user.service";
+
+@injectable()
+export class CommunityUserController {
+  constructor(
+    @inject(TYPES.CommunityUserService)
+    private communityUserService: ICommunityUserService
+  ) {}
+
+  async joinInCommunity(req: Request, res: Response): Promise<void> {
+    try {
+      const userId = req.user?.id as string;
+      const communityId = req.params.id;
+      const response = await this.communityUserService.joinInCommunity(
+        userId,
+        communityId
+      );
+      res.status(201).json(response);
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+}

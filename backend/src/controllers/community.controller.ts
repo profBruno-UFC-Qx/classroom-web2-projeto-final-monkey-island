d714c3aa820fd0b1864e14d073cf08fd1207d1ad
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-01 14:37:35 -0300

    feat: adding create community method in community controller

---
+++ backend/src/controllers/community.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import { inject, injectable } from "inversify";
+import { TYPES } from "../types/types";
+
+import { ICommunityService } from "../services/community.service";
+import { CommunityRequestDto } from "../dtos/community/request/community.request.dto";
+
+@injectable()
+export class CommunityController {
+  constructor(
+    @inject(TYPES.CommunityService) private communityService: ICommunityService
+  ) {}
+
+  async createCommunity(req: Request, res: Response): Promise<void> {
+    try {
+      const userId = req.user?.id as string;
+      const request: CommunityRequestDto = req.body;
+      const response = this.communityService.createCommunity(userId, request);
+      res.status(201).json(response);
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+}

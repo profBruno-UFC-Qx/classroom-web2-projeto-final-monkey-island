400fecff23b686e80cf429238fede046163f2538
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-01 14:40:44 -0300

    feat: adding update community data method in community controller

--- backend/src/controllers/community.controller.ts
+++ backend/src/controllers/community.controller.ts
@@ -4,6 +4,7 @@ import { TYPES } from "../types/types";
 
 import { ICommunityService } from "../services/community.service";
 import { CommunityRequestDto } from "../dtos/community/request/community.request.dto";
+import { CommunityUpdateRequestDto } from "../dtos/community/request/community.update.request.dto";
 
 @injectable()
 export class CommunityController {
@@ -17,6 +18,22 @@ export class CommunityController {
       const request: CommunityRequestDto = req.body;
       const response = this.communityService.createCommunity(userId, request);
       res.status(201).json(response);
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+
+  async updateCommunityData(req: Request, res: Response): Promise<void> {
+    try {
+      const userId = req.user?.id as string;
+      const communityId = req.params.id;
+      const request: CommunityUpdateRequestDto = req.body;
+      const response = this.communityService.updateCommunityData(
+        userId,
+        communityId,
+        request
+      );
+      res.status(200).json(response);
     } catch (error: any) {
       res.status(400).json({ message: error.message });
     }

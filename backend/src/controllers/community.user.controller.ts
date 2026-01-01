91621e30f4397f9771ca9997506ee977966b3c71
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-01 15:38:49 -0300

    feat: adding left of gropu method in communityUser controller

--- backend/src/controllers/community.user.controller.ts
+++ backend/src/controllers/community.user.controller.ts
@@ -24,4 +24,18 @@ export class CommunityUserController {
       res.status(400).json({ message: error.message });
     }
   }
+
+  async leftOfCommunity(req: Request, res: Response): Promise<void> {
+    try {
+      const userId = req.user?.id as string;
+      const communityId = req.params.id;
+      const response = await this.communityUserService.leftOfCommunity(
+        userId,
+        communityId
+      );
+      res.status(200).json(response);
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  }
 }

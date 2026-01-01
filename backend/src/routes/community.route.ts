6176d1daf7fcf7a688e1b68c88eb335f66dd4e33
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-01 19:44:31 -0300

    feat: adding community routes

--- backend/src/routes/community.route.ts
+++ backend/src/routes/community.route.ts
@@ -2,3 +2,42 @@ import { Router } from "express";
 import { container } from "../di/container.di";
 import { TYPES } from "../types/types";
 import { CommunityController } from "../controllers/community.controller";
+import { userIsResearcher } from "../middlewares/user.is.researcher";
+import { userAuthentication } from "../middlewares/user.autentication";
+
+const communityController: CommunityController = container.get(
+  TYPES.CommunityController
+);
+
+const communityRoutes = Router();
+
+communityRoutes.post(
+  "/community",
+  userAuthentication,
+  userIsResearcher,
+  (req, res) => communityController.createCommunity(req, res)
+);
+
+communityRoutes.delete("/community/:id", userAuthentication, (req, res) =>
+  communityController.deleteCommunity(req, res)
+);
+
+communityRoutes.put("/community/:id", userAuthentication, (req, res) =>
+  communityController.updateCommunityData(req, res)
+);
+
+communityRoutes.get(
+  "/community/created-for-me",
+  userAuthentication,
+  (req, res) => communityController.findCommunitiesCreatedByUser(req, res)
+);
+
+communityRoutes.get("/community/search", (req, res) =>
+  communityController.findCommunityByNameLike(req, res)
+);
+
+communityRoutes.get("/community/popular", (req, res) =>
+  communityController.findPopularCommunities(req, res)
+);
+
+export default communityRoutes;

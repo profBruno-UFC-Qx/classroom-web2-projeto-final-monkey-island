9c9aad573a0fbb63d470887ac51b8cf424b11091
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-06 19:30:34 -0300

    feat: adding save and delete methods in repository

--- backend/src/repositories/post.media.repository.ts
+++ backend/src/repositories/post.media.repository.ts
@@ -1,8 +1,25 @@
+import { injectable } from "inversify";
 import { PostMedia } from "../entities/post.media";
+import { AppDataSource } from "../config/db.connection";
 
 export interface IPostMediaRepository {
   save(postMedia: PostMedia): Promise<PostMedia>;
   delete(postMediaId: string): Promise<void>;
   getMediaById(mediaId: string): Promise<PostMedia>;
   getAllMediasByPost(postId: string): Promise<PostMedia[]>;
+}
+
+@injectable()
+export class PostMediaRepositoryDB implements IPostMediaRepository {
+  private get repo() {
+    return AppDataSource.getRepository(PostMedia);
+  }
+
+  async save(postMedia: PostMedia): Promise<PostMedia> {
+    return await this.repo.save(postMedia);
+  }
+
+  async delete(postMediaId: string): Promise<void> {
+    await this.repo.delete({ id: postMediaId });
+  }
 }

deb079da2987dcf3f545d1d7aaeae45cacd22987
Raul Ronald <108026640+RaulRonald@users.noreply.github.com>
2026-01-10 23:09:31 -0300

    consertando branch da login-page

--- backend/src/dtos/artifact/request/artifact.update.request.dto.ts
+++
@@ -1,7 +0,0 @@
-import { ArtifactRarity } from "../../../entities/artifact";
-
-export interface ArtifactUpdateRequestDto {
-  name?: string;
-  rarity?: ArtifactRarity;
-  description?: string;
-}
--- backend/src/dtos/auth/response/login.response.dto.ts
+++
@@ -1,3 +0,0 @@
-interface LoginResponseDto {
-  jwt: string;
-}
